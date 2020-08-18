208beb36463c3c30f855513850695767e2d11e2d
calvin Law <calvin.law@swivelsoftware.com>
2020-08-18 18:02:04 +0800

    data binding and class binding and list and components

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+    v-bind:class="{'is-complete':todo.compelted}"
+    
+    >
+         
+        <h3>{{todo.title}}</h3>
+        </div>
+</template>
+
+<script>
+export default {
+    name:"TodoItem",
+    props:['todo']
+}
+</script>
+
+<style scoped>
+.todo-item{
+    background:#f4f4f4;
+    padding:10px;
+    border-bottom:1px #ccc dotted;
+}
+
+.is-complete{
+    text-decoration: line-through;
+}
+
+
+
+</style>
